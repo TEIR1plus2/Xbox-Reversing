719065acf2c1776039573cde9ae24af2f3c6aace
teir <teir1plus2@live.com>
2018-01-01 14:38:17 -0800

    Update HV_Utility.c

--- HV_Utility.c
+++ HV_Utility.c
@@ -5,38 +5,42 @@
 // 17511
 // HV utility functions
 
+// These assume they are compiled for 64bit machines, where pointers are 64 bits
+// The HV handles everything in 64 bit mode
 
 // Xbox doesn't write cachelines back unless a dcbst instruction is used
 // or unless the data was being forced out of the cacheline
 // This will make sure your RAM is up to date with the cache
 // Used to write cachelines for a given memory range to RAM
-void HvpSaveCachelines(QWORD* pAddy, DWORD cAddy);
+void HvpSaveCachelines(PVOID pAddy, DWORD cAddy);
 
 // Writes zeroed cache blocks directly to RAM without 
 // loading RAM into cache, zeroing, writing back to memory
 // Efficient way to zero memory blocks
-void HvpZeroCacheLines(QWORD pAddy, DWORD cAddy);
+// Using the dbcz128 instruction effectively is documented in the SDK
+void HvpZeroCacheLines(PVOID pAddy, DWORD cAddy);
 
 // Used to invalidate cachelines for a given address range
 // Takes byte count as cAddy input, NOT CACHELINE COUNT
-void HvpInvalidateCachelines(QWORD pAddy, DWORD cAddy);
+void HvpInvalidateCachelines(PBYTE pAddy, DWORD cbAddy);
 
 // Relocates data in 0x80 byte blocks and invalidates the old location
-void HvpRelocateCacheLines(QWORD* pSrc, QWORD* pDest, DWORD cCount);
+void HvpRelocateCacheLines(PVOID pSrc, PVOID pDest, DWORD cBlockCount);
 
-// Move data to an internal memory area, return new address
-QWORD HvpRelocatePhysicalToInternal(QWORD pbBuf, DWORD cbBuf, BYTE bPage);
+// Sets address flags and re-writes with the new flags, returns new address
+QWORD HvpRelocatePhysicalToEncrypted(PBYTE pbBuf, DWORD cbBuf);
+QWORD HvpRelocatePhysicalToProtected(PBYTE pbBuf, DWORD cbBuf, BYTE bKF);
 
 // Get the real address of a physical address
 // The HV works with memory paging disabled
-QWORD HvpPhysicalToReal(DWORD pAddy, DWORD cAddy);
+QWORD HvpPhysicalToReal(PVOID pAddy, DWORD cAddy);
 
 // Gets non-zero timebase value
 QWORD HvpGetTimebase();
 
 QWORD HvpGetFlashBaseAddress();
-QWORD HvpGetSocMMIORegs(DWORD offset);
+QWORD HvpBuildSocMMIORegs(DWORD offset) ;
 QWORD HvpBuildPciConfigRegs(DWORD offset);
-QWORD HvpGetHostBridgeRegs(DWORD offset);
+QWORD HvpBuildHostBridgeRegs(DWORD offset);
 
 // work in progress...
